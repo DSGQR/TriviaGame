c0c8ee8947979198ff0b7a7ad17d2087f949813b
DAmini <amini.daniel@gmail.com>
2018-08-30 10:24:25 -0700

    Trivia Questions Added

---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+let questions = [ 
+    {
+        question: 'What percentage of all photos in the world have been taken in the past 12 months.',
+        choices: ['40%', '15%', '10%', '25%'],
+        correct: '10%',
+    },
+    {
+        question: 'Over 7000 US citizens die and 1.5 million impair their health due to __________.',
+        choices: ['Routine Vaccines', 'Poor Healthcare Customer Service', 'Illegible Doctors Writing', 'Inconsiderate Patient Staff'],
+        correct: 'Illegible Doctors Writing',
+    },
+    {
+        question: 'Most diamonds are at least 3 _______ years old.',
+        choices: ['Hundred', 'Billion', 'Million', 'Thousand'],
+        correct: 'Billion',
+    },
+    {
+        question: 'Every 10th European child has been conceived in a(n) ______ bed.',
+        choices: ['Sklar', 'Macys', 'Walmart', 'IKEA'],
+        correct: 'IKEA',
+    },
+    {
+        question: 'Heroin was initially advertised as a cure for _______.',
+        choices: ['Coughs', 'The Common Cold', 'Malaria', 'STDs'],
+        correct: 'Coughs',
+    },
+    {
+        question: 'What Percentage of mankind dont live to see their first birthday?',
+        choices: ['10%', '20%', '30%', '40%'],
+        correct: '40%',
+    },
+    {
+        question: 'Moon dust smells of ________.',
+        choices: ['Tar', 'Gunpowder', 'Resin', 'Thermal Paste'],
+        correct: 'Gunpowder',
+    },
+    {
+        question: 'How many teeth does a bear have?',
+        choices: ['50', '48', '42', '38'],
+        correct: '42',
+    },
+    {
+        question: 'What month has the highest percentage of births?',
+        choices: ['August', 'February', 'December', 'June'],
+        correct: 'August',
+    },
+    {
+        question: 'When lightning strikes it can reach up to ________ degrees fahrenheit.',
+        choices: ['30,000', '25,000', '60,000', '54,000'],
+        correct: '54,000',
+    },
+]
